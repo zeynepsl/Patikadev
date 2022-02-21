88a8b82e5440edb5c34a13d84fa0186badea452f
zeynepsl <zeyneprsalman@gmail.com>
2022-02-21 18:31:32 +0300

    kdv hesaplayan program

---
+++ patikadev/src/java101/KDVTutariHesaplama.java
@@ -0,0 +1,26 @@
+package java101;
+
+import java.util.Scanner;
+
+public class KDVTutariHesaplama {
+	/*
+	 * KDV Tutarý Hesaplayan Program Java ile kullanýcýdan alýnan para deðerinin
+	 * KDV'li fiyatýný ve KDV tutarýný hesaplayýp ekrana bastýran programý yazýn.
+	 * 
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double kdv = 0.18, fiyat, kdvliFiyat, kdvTutari;
+
+		System.out.println("KDV'siz Fiyatý girin: ");
+		fiyat = scanner.nextDouble();
+
+		kdv = fiyat > 1000 ? 0.8 : 0.18;
+
+		kdvTutari = kdv * fiyat;
+		kdvliFiyat = kdvTutari + fiyat;
+
+		System.out.println("Girilen " + fiyat + " degerinin kdvli fiyatý: " + kdvliFiyat + "\nkdv tutarý: " + kdvTutari);
+
+	}
+}
