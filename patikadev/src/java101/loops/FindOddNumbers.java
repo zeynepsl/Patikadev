a593c024b210849638f7e1c399702f7d6a07e1aa
zeynepsl <zeyneprsalman@gmail.com>
2022-03-01 18:38:03 +0300

    tek sayilar toplami

--- patikadev/src/java101/loops/FindOddNumbers.java
+++ patikadev/src/java101/loops/FindOddNumbers.java
@@ -3,25 +3,30 @@ package java101.loops;
 import java.util.Scanner;
 
 public class FindOddNumbers {
-	/*Java döngüler ile negatif bir deðer girilene kadar kullanýcýdan giriþleri kabul eden 
-	 * ve girilen deðerlerden tek sayýlarý toplayýp ekrana basan programý yazýyoruz.*/
+	/*
+	 * Java döngüler ile negatif bir deðer girilene kadar kullanýcýdan giriþleri
+	 * kabul eden ve girilen deðerlerden tek sayýlarý toplayýp ekrana basan programý
+	 * yazýyoruz.
+	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("sayiyi girin: ");
-		int num = scanner.nextInt();
-		
+		int num;
 		int total = 0;
-		
-		while(num != -1) {
-			if(num % 2 != 0) {
-				total += num;
-			}
+
+		do {
 			System.out.println("sayiyi girin: ");
 			num = scanner.nextInt();
-		}
-		
+			if (num % 2 == 1) {
+				total += num;
+			}
+		} while (num > 0);
+
+		/*
+		 * while(num != -1) { if(num % 2 != 0) { total += num; }
+		 * System.out.println("sayiyi girin: "); num = scanner.nextInt(); }
+		 */
+
 		System.out.println("toplam: " + total);
 	}
-	//Her zorluðun bir mükafatý vardýr. :)))
+	// Her zorluðun bir mükafatý vardýr. :)))
 }
