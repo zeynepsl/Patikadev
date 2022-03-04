5192a219bfa8195730caa52aea1727c9f1e7a2a3
zeynepsl <zeyneprsalman@gmail.com>
2022-03-04 13:50:24 +0300

    comparator, comparable studies

---
+++ patikadev/src/ComparatorComparable/ComparingIntMethod.java
@@ -0,0 +1,61 @@
+package ComparatorComparable;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparingIntMethod {
+	public static void main(String[] args) {
+
+		// create some user objects
+		User u1 = new User("Aaman", 25);
+		User u2 = new User("Joyita", 22);
+		User u3 = new User("Suvam", 28);
+		User u4 = new User("mahafuj", 25);
+
+		// before sort
+		List<User> list = Arrays.asList(u1, u2, u3, u4);
+		System.out.println("Before Sort:");
+		list.forEach(User -> System.out.println("User age " + User.getAge()));
+
+		Collections.sort(list, Comparator.comparingInt(User::getAge));
+		System.out.println("\nAfterSort:");
+		list.forEach(User -> System.out.println("User age " + User.getAge()));
+	}
+}
+
+class User implements Comparable<User> {
+	public String name;
+	public int age;
+
+	public User(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public int compareTo(User u1) {
+		return name.compareTo(u1.name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+}
